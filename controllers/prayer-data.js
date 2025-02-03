57a1c28763cdc25e56c63f300b7c6be8138cbba5
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-02-03 23:42:03 +0600

    added get full prayer time api

--- controllers/prayer-data.js
+++ controllers/prayer-data.js
@@ -1,7 +1,62 @@
 import { calendar, getPrayerTimes } from "prayer-time-bd";
+const {
+  startOfMonth,
+  endOfMonth,
+  format,
+  parse,
+  differenceInDays,
+  closestTo,
+} = require("date-fns");
+const { schedule } = calendar;
+
+const getMonthDetails = (month, year = new Date().getFullYear()) => {
+  const firstDate = startOfMonth(new Date(year, month));
+  const lastDate = endOfMonth(firstDate);
+  const monthName = format(firstDate, "MMMM");
+
+  return { monthName, firstDate, lastDate };
+};
+
+const generateMonthSpecificSchedules = (monthIndex) => {
+  const { firstDate, lastDate, monthName } = getMonthDetails(monthIndex);
+  const monthSchedule =
+    schedule.months.find((m) => m.name === monthName)?.schedule || [];
+
+  // Extracting available schedule dates
+  const availableDates = monthSchedule.map((s) =>
+    parse(s.date, "dd", firstDate)
+  );
+
+  const findNearestSchedule = (dayDate) => {
+    if (availableDates.length === 0) return null;
+
+    const closestDate = closestTo(dayDate, availableDates);
+    const formattedDate = format(dayDate, "dd"); // Convert to match schedule format
+
+    return (
+      monthSchedule.find((s) => s.date === formattedDate) || {
+        date: formattedDate,
+        message: "No available schedule",
+      }
+    );
+  };
+
+  const schedules = [];
+  let currentDate = firstDate;
+
+  while (currentDate <= lastDate) {
+    const scheduleForDay = findNearestSchedule(currentDate);
+    schedules.push({ ...scheduleForDay, date: format(currentDate, "dd") });
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return { monthName, schedules };
+};
 
 function getPermanentPrayerTimes() {
-  return calendar;
+  return Array.from({ length: 12 }, (_, i) =>
+    generateMonthSpecificSchedules(i)
+  );
 }
 
 function getPrayerTimesByDateAndDistrict(
