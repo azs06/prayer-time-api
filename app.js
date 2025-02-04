07f557e9ba253ad1d9cf62357fc8cb1d23a116d9
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-02-05 03:31:52 +0600

    added default fallback port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { getPermanentPrayerTimes, getPrayerTimesByDateAndDistrict } from "./cont
 import { formatDate } from "./utils/date.js";
 import 'dotenv/config'
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3459;
 
 const app = express();
 
